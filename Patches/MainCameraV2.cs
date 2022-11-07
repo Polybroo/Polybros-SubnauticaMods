1231a2bcf0f1b4e7da555d7ffaaf82774d71c96c
Fokusnikh <96922685+Polybroo@users.noreply.github.com>
2022-11-07 20:51:38 +0100

    created viewmodel camera

---
+++ Patches/MainCameraV2.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Polynautica
+{
+	class MainCameraV2Patch
+	{
+		[HarmonyPatch(typeof(MainCameraV2))]
+		[HarmonyPatch("Start")]
+		internal class Start
+		{
+			[HarmonyPostfix]
+			public static void Postfix(MainCameraV2 __instance)
+			{
+				var traverse = Traverse.Create(__instance);
+				Traverse<int> dcm = traverse.Field<int>("defaultCullingMask");
+				//dcm.Value &= ~(1 << 8);
+				//__instance.RestoreCullingMask();
+
+				GameObject copy = GameObject.Instantiate(SNCameraRoot.main.guiCam.gameObject);
+
+				Transform parent = SNCameraRoot.main.mainCam.transform.parent;
+				copy.transform.parent = parent;
+				copy.transform.localEulerAngles = Vector3.zero;
+				copy.transform.localPosition = Vector3.zero;
+				Camera cam = copy.GetComponent<Camera>();
+				cam.cullingMask = LayerMask.GetMask("Viewmodel");
+				cam.clearFlags = CameraClearFlags.Depth;
+				cam.depth = 2;
+				cam.depthTextureMode = DepthTextureMode.None & DepthTextureMode.Depth & DepthTextureMode.DepthNormals & DepthTextureMode.MotionVectors;
+				cam.renderingPath = RenderingPath.DeferredShading;
+				cam.enabled = false;
+			}
+		}
+	}
+}
